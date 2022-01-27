ee7ae3b769d94f1f3ac33bc5c41578ecb197c2dc
MikeShahin <gobias101@gmail.com>
2022-01-27 10:53:14 -0800

    updated app.js, added trips page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import { connect } from 'react-redux'
 import Home from './components/Home';
+import AddTrip from './containers/AddTrip'
 import './App.css';
 
 class App extends Component {
