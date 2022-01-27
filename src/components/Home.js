0ca11ecc687606d95b39186661170a931fada003
MikeShahin <gobias101@gmail.com>
2022-01-27 10:49:47 -0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,16 @@ class Home extends React.Component {
           <div>
             <h1>Welcome to Plane View, The site that lets you figure out the best view for your next trip!</h1>
             <p>Construction in progress</p>
+
+            <button placeholder="submit" type="submit">
+              view trips
+            </button>
+
+            <br>
+            
+            <button placeholder="submit" type="submit">
+              add trip
+            </button>
           </div>
       </div>
     )
