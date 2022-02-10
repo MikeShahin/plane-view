5a088db7279f630b7c122d456d48c4454c08db18
MikeShahin <gobias101@gmail.com>
2022-02-10 11:18:25 -0800

    Create ViewTrips.js

---
+++ src/components/ViewTrips.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ViewTrips extends React.Component {
+  
+
+  render() {
+    return (
+      <div className="trips center">
+
+      </div>
+    )
+  }
+}
+
+
+export default (ViewTrips); 
