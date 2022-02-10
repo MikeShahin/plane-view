e4fd573e332f78659331c2e9c7f6cc64979059d1
MikeShahin <gobias101@gmail.com>
2022-02-10 11:20:32 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 // import { connect } from 'react-redux'
 import Home from './components/Home';
 import NewTrip from './containers/NewTrip'
+import ViewTrips from './components/ViewTrips';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <Switch>
             {/* <Route exact path='/' component={Home}/> */}
             <Route exact path='/newtrip' component={NewTrip}/>
+            <Route exact path='/viewtrips' component={ViewTrips}/>
           </Switch>
         </BrowserRouter>
       </div>
