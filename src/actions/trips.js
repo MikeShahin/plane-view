fd097d72d67092e9b4ac3dc51a26011383ed471c
MikeShahin <gobias101@gmail.com>
2022-02-24 12:05:59 -0800

    added new trip action

--- src/actions/trips.js
+++ src/actions/trips.js
@@ -8,5 +8,26 @@ export function fetchTrips() {
 }
 
 export function addTrip(tripInfo) {
-
+    return dispatch => {
+        return fetch("http://localhost:3001/trips", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({dive: tripInfo})
+        })
+        .then(r => r.json())
+        .then(response => {
+            if (response.error) {
+                alert("something went wrong!")
+            } else {
+                dispatch({
+                    type: "CREATE_TRIP",
+                    dive: response
+                })
+                alert("Trip Added!")
+            }
+        })
+    }
 }
