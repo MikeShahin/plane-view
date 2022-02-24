7b2364a56db8c3625d19d5dfdf860fd5bc200149
MikeShahin <gobias101@gmail.com>
2022-02-24 12:05:02 -0800

    added show trips action

--- src/actions/trips.js
+++ src/actions/trips.js
@@ -1,5 +1,10 @@
 export function fetchTrips() {
-
+    return (dispatch) => {
+        return fetch('http://localhost:3001/trips/')
+        .then(response => response.json())
+        .then(json => dispatch(
+            { type: "SHOW_TRIPS", data: json }))
+        }
 }
 
 export function addTrip(tripInfo) {
