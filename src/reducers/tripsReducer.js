f818ed065170f2935f6e6e88f440686c80eaec7f
MikeShahin <gobias101@gmail.com>
2022-02-24 12:08:22 -0800

    added reducers for showing and creating trips

--- src/reducers/tripsReducer.js
+++ src/reducers/tripsReducer.js
@@ -1,3 +1,16 @@
 export const tripsReducer = (state = {}, action) => {
+    switch(action.type) {
 
+        case 'SHOW_TRIPS':
+            return { ...state, data: action.data }
+
+        case 'CREATE_TRIP':
+            return {
+                ...state,
+                trip: action.trip
+            }
+        
+        default:
+            return state
+    }
 }
