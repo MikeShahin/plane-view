f73507a3edd93db9b1c5eab9fff3d344e217e5e5
MikeShahin <gobias101@gmail.com>
2022-03-10 12:04:49 -0800

    Update TripsCard.js

--- src/components/TripsCard.js
+++ src/components/TripsCard.js
@@ -5,7 +5,13 @@ const TripsCard = props => {
   
     return (
         <div className="tripcard">
-
+            {/* code for pics */}
+            <div className="divecard-text">
+            <h3>Trip # {props.id}</h3>
+                <p><strong>Route: </strong>{props.route}, <strong>date: </strong>{moment(props.date).format("MMM Do YYYY")}, <strong>local time: </strong>{props.local_time}</p>
+                <p><strong>Side of plane: </strong>{props.side}</p>
+                <p><strong>Comments:</strong><br></br>{props.comments}</p>
+            </div>
         </div>
     )
 }
