59520e0c1465712804af4d6a5a1509635376148b
MikeShahin <gobias101@gmail.com>
2022-03-16 12:11:53 -0700

    Create AllTrips.js

---
+++ src/containers/AllTrips.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class AllTrips extends Component {
+
+    render() {
+        return (
+            <div className="trips">
+
+            </div>
+        )
+    }
+    
+}
+
+}
+
+export default AllTrips
