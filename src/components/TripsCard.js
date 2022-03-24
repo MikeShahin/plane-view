b35c774754c492d884db1119ccd0933b7878d2c7
MikeShahin <gobias101@gmail.com>
2022-03-24 11:42:55 -0700

    updated trips card

--- src/components/TripsCard.js
+++ src/components/TripsCard.js
@@ -5,9 +5,14 @@ const TripsCard = props => {
   
     return (
         <div className="tripcard">
-            {/* code for pics */}
-            <div className="divecard-text">
-            <h3>Trip # {props.id}</h3>
+            {props.pictures !== "" && 
+                <img className="tripcard-image" src={props.pictures} alt="dive pic"/>
+            }
+            {props.pictures === "" && 
+                <img className="tripcard-image" src="#" alt="dive pic"/>
+            }
+            <div className="tripcard-text">
+            <h3>Trip # {props.id}, by: {props.user}</h3>
                 <p><strong>Route: </strong>{props.route}, <strong>date: </strong>{moment(props.date).format("MMM Do YYYY")}, <strong>local time: </strong>{props.local_time}</p>
                 <p><strong>Side of plane: </strong>{props.side}</p>
                 <p><strong>Comments:</strong><br></br>{props.comments}</p>
--- src/containers/NewTrip.js
+++ src/containers/NewTrip.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Redirect } from "react-router";
 
 class NewTrip extends React.Component {
   state = {
